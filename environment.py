14b3521ab911e74c30bb3a91446d65a981d1c819
Denaldo Lapi <denaldo98@gmail.com>
2022-05-07 12:41:01 +0200

    IQL

--- environment.py
+++ environment.py
@@ -1,19 +1,19 @@
 import random
 import numpy as np
-from dataclasses import dataclass, field
 
-# grid word environment
-Pair = tuple[int, int]
-Action = int
+# set seed for reproducible experiments
+random.seed(1)
 
-epsilon = 0.6
+# Q-LEARNING PARAMETERS
+epsilon = 0.1
 alpha = 0.1 # decreasing
 gamma = 0.9 # fixed
 
-
+# ACTIONS
 ACTIONS = (0, 1, 2, 3, 4)
 ACTION_TO_STRING = ("up", "down", "left", "right", "stay")
-ACTION_TO_PAIR = ((-1, 0), (1, 0), (0, -1), (0, 1), (0, 0))
+ACTION_TO_PAIR = ((1, 0), (-1, 0), (0, -1), (0, 1), (0, 0))
+
 
 class Environment:
     
@@ -21,11 +21,10 @@ class Environment:
     def __init__(self, N):
         self.N = N
         self.size = (N, N) # grid dimensions
-
         self.num_states = N * N * N * N * N * N # each state is <x1, y1, x2, y2>
         self.num_actions = 5 
 
-        # create predators
+        # create predators/agents
         self.preds = [Agent(self.num_states, self.num_actions) for _ in range(2)]
 
 
@@ -33,7 +32,7 @@ class Environment:
     def randpair(self):
         return (random.randrange(self.N), random.randrange(self.N))
     
-    # Initialize randomly the state of the environment 
+    # Randomly the state, i.e. coordinates of predators and prey 
     def reset(self):
         self.pred_locs = tuple([self.randpair() for _ in range(2)]) # each el is a tuple with the coordinates 
         self.prey_loc = tuple([self.randpair()]) # single tuple with prey coordinates
@@ -43,13 +42,15 @@ class Environment:
         # get the state index
         h = self.state_index()
 
-        # get predator's location
+        # get predator's locations
         pred_locs = list(self.pred_locs)
 
+        chosen_actions = np.zeros(2, dtype=int)
         # iterate over the predators
         for i, pred in enumerate(self.preds):
             # predator selects action in current state
-            action = pred.choose(h)
+            action = pred.select_action(h)
+            chosen_actions[i] = int(action)
             #perform movement
             pred_locs[i] = move(pred_locs[i], action, self.size)
     
@@ -63,48 +64,54 @@ class Environment:
             reward = 0
 
         # now, update Q-values
+        self.update_Q(reward)
+
+        # return the reward
+        return reward, chosen_actions
+    
+    # Apply Q-learning update formula
+    def update_Q(self, reward):
         for i, pred in enumerate(self.preds):
-            old_value =  pred.Q[pred.prev_state, pred.prev_action]
-            index_new_state= self.state_index()
+            old_value = pred.Q[pred.prev_state, pred.prev_action]
+            index_new_state = self.state_index()
             next_max = np.max(pred.Q[index_new_state])
             # Q-learning update
             pred.Q[pred.prev_state, pred.prev_action] = (1 - alpha) * old_value + alpha * (reward + gamma * next_max) 
 
-            return reward
-
+    '''
     def hash(self):
         # Environment state can be hashed for use in agent's Q-table
         return hash((self.pred_locs, self.prey_loc))
-    
+    '''
+
     def state_index(self):
         return self.pred_locs[0][0]*self.pred_locs[0][1]*self.pred_locs[1][0]*self.pred_locs[1][1]*self.prey_loc[0][0]*self.prey_loc[0][1]
-
-
-
     
-    
-
     def __repr__(self) -> str:
         # display environment as a grid
         grid = [[" "] * self.size[1] for _ in range(self.size[0])]
         for i, pdl in enumerate(self.pred_locs):
             if i == 0:
-                grid[pdl[0]][pdl[1]] = "X"
+                grid[pdl[0]][pdl[1]] = "X" # first agent represented with an 'X'
             if i == 1:
-                grid[pdl[0]][pdl[1]] = "Y"
+                grid[pdl[0]][pdl[1]] = "Y" # second agent represented with a 'Y'
         for prl in self.prey_loc:
             grid[prl[0]][prl[1]] = "O"
+
+        # ADD NEW SYMBOLS FOR PREDS IN THE SAME CELL, OR FOR PRED AND PREY IN SAME CELL, OR ALL IN THE SAME CELL
 
         return (
             ("_" * self.size[1] * 2 + "\n")
             + "\n".join("|" + " ".join(row) + "|" for row in grid)
             + ("\n" + " ̅" * self.size[1] * 2 + "\n")
         )
+    
+    # return current state
+    def get_positions(self):
+        return self.pred_locs[0], self.pred_locs[1], self.prey_loc[0]
 
 
-
-
-# simple Q-learning agent
+# IQL agent
 class Agent:
     def __init__(self, states, actions):
     
@@ -122,12 +129,16 @@ class Agent:
         #self.n_visits_sa = np.zeros([states, actions])
     
     # select action according to epsilon greedy approach
-    def choose(self, state):
+    def select_action(self, state):
         rand = random.uniform(0, 1)
+        
+        # apply epsilon-greedy policy
         if rand < epsilon:
             action = random.choice(ACTIONS) # choose random action inside the action space
         else:
             action = np.argmax(self.Q[state]) # best action in current state
+
+        # save initial state and chosen action    
         self.prev_action = action
         self.prev_state = state
         return action
@@ -136,11 +147,12 @@ class Agent:
 
 # auxiliary function that moves the agent on the grid to a new position(coordinates)
 def move(start, action, size):
-    dir = ACTION_TO_PAIR[action]
-    result = start[0] + dir[0], start[1] + dir[1]
+    dir = ACTION_TO_PAIR[action] # convert action into pair
+    final_position = start[0] + dir[0], start[1] + dir[1] # obtain new position by summing coordinates
 
     # in case of forbidden action, stay in the same position
-    if not (0 <= result[0] < size[0] and 0 <= result[1] < size[1]):
+    if not (0 <= final_position[0] < size[0] and 0 <= final_position[1] < size[1]):
         return start
+
     # return final position
-    return result
+    return final_position
--- new_main.py
+++ new_main.py
@@ -1,35 +1,91 @@
-# TRIALS
-import numpy as np
-import random
+import matplotlib
 import environment
-import utils
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-darkgrid')
+plt.rcParams["figure.figsize"] = (12, 6)
+
+
 
 # define grid size
 N = 4
-
+ 
 # create environment by passing the grid size
 env = environment.Environment(N)
 
 
 # TRAINING PROCEDURE
 
-# TRAIN JUST 1 EPISODE
+# num of TRAINING episodes
+n_episodes = 100
+
+# max steps of each episode
+max_steps = 5000
+
+# Time to goal
+time_goal = []
+
+
+
+# TRAINING LOOP
 print("STARTING TRAINING")
-train_steps = 1000
 
-env.reset() # randomly initialize agents positioninh
+start_time = time.time()
+for episode in range(n_episodes):
+    
+    env.reset() # randomly initialize the state of the env
+    
+    goal = 0 # encodes the reaching of a goal state (1 if goal state)
 
-for i in range(train_steps):
-    if i % (train_steps // 20) == 0:
-            print(f"Completed {i} / {train_steps} steps")
-    env.transition()
+    performed_steps = 0
 
+    # run episode until reaching goal state or max_steps
+    while(not goal) and (performed_steps < max_steps):
+        goal, _ = env.transition() # returns 1 if goal state is reached
+        performed_steps +=1
+    
+    # at the end of each episode we add to the list the time to goal
+    time_goal.append(performed_steps)
+
+    if episode % 20 == 0:
+        print(f"Completed {episode} / {n_episodes} episodes")
+
+end_time = time.time()
+print("TRAINING FINISHED") 
+print("\nTotal required time: {} seconds, {} minutes".format(end_time - start_time, (end_time - start_time)/60))
+
+
+
+# PLOT THE TIME TO GOAL
+print("Plot of the Time To Goal over the episodes")
+plt.plot(range(n_episodes), time_goal)
+plt.title("Time To Goal over the episodes")
+plt.xlabel("Training Episodes")
+plt.ylabel("Time To Goal")
+plt.tight_layout()
+plt.show()
+
+
+# SIMULATION (with the learned Q-values)
+reward = "Initial configuration: NO REWARD"
+action = np.array(["No action", "No action"])
+time_step = 0
+goal = 0
 print("Now watch environment proceed step-by-step")
-while True:
+while not goal:
     print(env)
-    #print(env.reward_of())
-    env.transition()
-    input("Press enter")
+    print("Timestep: {}".format(time_step))
+    print("Predators and prey coordinates: {}".format(env.get_positions()))
+    print("Action chosen by first predator: {}".format(action[0]))
+    print("Action chosen by second predator: {}".format(action[1]))
+    print("Reward: {}".format(reward))
+    
+
+    reward, action = env.transition()
+    goal = reward
+    time_step += 1
+    input("Press enter to visualize next state")
 
 
 
